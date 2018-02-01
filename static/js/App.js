58656f9964dbfc99d53b03f4a32317e40039c9e3
Johnny Dang <johnnydang92@gmail.com>
2018-02-01 23:50:26 +0100

    Merge fix

--- static/js/App.js
+++ static/js/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 export default class App extends React.Component {
   render () {
-    return <p> Hello React! gröt. Din boon</p>;
+    return <div><p> Hello React! Du är ful. Din boon </p> <br/> <img src="./images/banner.jpg" alt="Mountain View"></img></div>;
   }
 }
