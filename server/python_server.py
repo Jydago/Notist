7d913aa412c249285224162aa6f89a7705f9d03c
Johnny Dang <johnnydang92@gmail.com>
2018-05-14 00:16:13 +0200

    Python serves static assets. Flask templating. Conda env template

--- server/python_server.py
+++ server/python_server.py
@@ -1,15 +1,25 @@
 # server.py
-from flask import Flask, render_template
+from os import environ, path
+from flask import Flask, render_template, send_from_directory
+
+
+here = path.abspath(path.dirname(__file__))
+print(here)
 
 app = Flask(__name__, static_folder="../static/dist", template_folder="../static")
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    message = 'This is from da python'
+    return render_template("index.html", message=message)
 
 @app.route("/hello")
 def hello():
-    return "Hello World Bobbo!"
+    return "Hello World Bobbo bobobob!"
+
+@app.route("/images/<path:filename>")
+def send_asset(filename):
+    return send_from_directory(path.join(here, "../static/images"), filename)
 
 if __name__ == "__main__":
     app.run()
--- static/js/App.js
+++ static/js/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 export default class App extends React.Component {
   render () {
-    return <div><p> Hello React! Du är ful. Din boon </p> <br/> <img src="./images/banner.jpg" alt="Mountain View"></img></div>;
+    return <div><p> Hello React! Hoppas det funkar...! </p> <br/> <img src="/images/banner.jpg" alt="Mountain View"></img></div>;
   }
 }
